e360b56f9f23a368e788ef16091ec987001a8253
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-21 19:23:50 +0100

    Handle dates which are in the previous solar year + year transition leap years

--- modules/bazi/src/main/kotlin/net/twisterrob/astro/bazi/lookup/EarthlyBranch_atSolarDay.kt
+++ modules/bazi/src/main/kotlin/net/twisterrob/astro/bazi/lookup/EarthlyBranch_atSolarDay.kt
@@ -3,6 +3,7 @@ package net.twisterrob.astro.bazi.lookup
 import net.twisterrob.astro.bazi.model.EarthlyBranch
 import java.time.LocalDate
 import java.time.Month
+import java.time.chrono.IsoChronology
 
 /**
  * References:
@@ -10,10 +11,10 @@ import java.time.Month
  */
 public fun EarthlyBranch.Companion.monthAtSolarDay(date: LocalDate): EarthlyBranch {
 	val reference = date.withYear(0)
-		.let { if (it < LocalDate.of(0, Month.FEBRUARY, 4)) it.plusYears(1) else it }
+		.let { if (it < solarYearStart(date.year).withYear(0)) it.plusYears(1) else it }
 	// TODO account for leap years.
 	return when (reference) {
-		in LocalDate.of(0, Month.FEBRUARY, 4)..<LocalDate.of(0, Month.MARCH, 6) -> EarthlyBranch.Yin
+		in solarYearStart(date.year).withYear(0)..<LocalDate.of(0, Month.MARCH, 6) -> EarthlyBranch.Yin
 		in LocalDate.of(0, Month.MARCH, 6)..<LocalDate.of(0, Month.APRIL, 5) -> EarthlyBranch.Mao
 		in LocalDate.of(0, Month.APRIL, 5)..<LocalDate.of(0, Month.MAY, 6) -> EarthlyBranch.Chen
 		in LocalDate.of(0, Month.MAY, 6)..<LocalDate.of(0, Month.JUNE, 6) -> EarthlyBranch.Si
@@ -24,7 +25,15 @@ public fun EarthlyBranch.Companion.monthAtSolarDay(date: LocalDate): EarthlyBran
 		in LocalDate.of(0, Month.OCTOBER, 8)..<LocalDate.of(0, Month.NOVEMBER, 7) -> EarthlyBranch.Xu
 		in LocalDate.of(0, Month.NOVEMBER, 7)..<LocalDate.of(0, Month.DECEMBER, 7) -> EarthlyBranch.Hai
 		in LocalDate.of(0, Month.DECEMBER, 7)..<LocalDate.of(1, Month.JANUARY, 6) -> EarthlyBranch.Zi
-		in LocalDate.of(1, Month.JANUARY, 6)..<LocalDate.of(1, Month.FEBRUARY, 4) -> EarthlyBranch.Chou
+		in LocalDate.of(1, Month.JANUARY, 6)..solarYearStart(date.year + 1).withYear(1) -> EarthlyBranch.Chou
 		else -> error("Unknown date: ${date} -> ${reference}")
+	}
+}
+
+public fun solarYearStart(year: Int): LocalDate {
+	return if (IsoChronology.INSTANCE.isLeapYear((year - 1).toLong())) {
+		LocalDate.of(year, Month.FEBRUARY, 4)
+	} else {
+		LocalDate.of(year, Month.FEBRUARY, 5)
 	}
 }
