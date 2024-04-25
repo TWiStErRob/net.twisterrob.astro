927b7ce56ceff742470538b83fca2906c6302f9f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-25 23:39:24 +0100

    Initial conversion of text to code

--- modules/bazi-calculator-manual/src/main/kotlin/net/twisterrob/astro/bazi/SolarCoordinateApproximator.kt
+++ modules/bazi-calculator-manual/src/main/kotlin/net/twisterrob/astro/bazi/SolarCoordinateApproximator.kt
@@ -1,6 +1,10 @@
 package net.twisterrob.astro.bazi
 
 import java.time.LocalDateTime
+import kotlin.math.asin
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
 
 internal class SolarCoordinateApproximator {
 
@@ -9,14 +13,47 @@ internal class SolarCoordinateApproximator {
 	 */
 	@Suppress(
 		"LocalVariableName", // Using names from math.
-		"UNUSED_VARIABLE", // STOPSHIP WIP
 	)
 	internal fun approximateSolarLongitude(dateTime: LocalDateTime): Double {
+		// Unit: Julian days
 		val JD = dateTime.julianDay
+		// Elapsed since J2000 epoch.
+		// Unit: Julian days
 		val D = JD - J2000_0
-		return 0.0
+		// Mean anomaly of the Sun
+		// Unit: degrees; Range: might not be 0-360.
+		val g = 357.529 + 0.98560028 * D
+		// Mean longitude of the Sun
+		// Unit: degrees; Range: might not be 0-360.
+		val q = 280.459 + 0.98564736 * D
+		// Geocentric apparent ecliptic longitude of the Sun (adjusted for aberration)
+		// Unit: degrees; Range: might not be 0-360.
+		val L = q + 1.915 * sin(g) + 0.020 * sin(2 * g)
+		// Sun's ecliptic latitude
+		val b = 0
+		// The distance of the Sun from the Earth
+		// Unit: astronomical units (AU)
+		val R = 1.00014 - 0.01671 * cos(g) - 0.00014 * cos(2 * g)
+		// Mean obliquity of the ecliptic
+		// Unit: degrees
+		val e = 23.439 - 0.00000036 * D
+		// Sun's right ascension (celestial equivalent of longitude) 
+		// tan(RA) = cos(e) * sin (L) / cos (L)
+		// RA is always in the same quadrant as L.
+		// If RA is obtained in degrees, it can be converted to hours simply by dividing by 15.
+		// RA is conventionally reduced to the range 0h to 24h.
+		val RA = atan2(cos(e) * sin(L), cos(L))
+		// the Sun's declination 
+		// sin(d) = sin(e) * sin(L)
+		val d = asin(sin(e) * sin(L))
+		// The Equation of Time, apparent solar time minus mean solar time
+		val EqT = q / 15 - RA
+		// angular semidiameter of the Sun
+		// Unit: degrees
+		val SD = 0.2666 / R
+		return RA
 	}
-	
+
 	companion object {
 
 		/**
---
+++ modules/bazi-calculator-manual/src/test/kotlin/net/twisterrob/astro/bazi/SolarCoordinateApproximatorTest.kt
@@ -0,0 +1,31 @@
+package net.twisterrob.astro.bazi
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.time.LocalDateTime
+
+class SolarCoordinateApproximatorTest {
+
+	private val subject = SolarCoordinateApproximator()
+
+	@CsvSource(
+		// Timings from https://en.wikipedia.org/wiki/March_equinox
+		"2019-03-20T21:58:00",
+		"2020-03-20T03:50:00",
+		"2021-03-20T09:37:00",
+		"2022-03-20T15:33:00",
+		"2023-03-20T21:25:00",
+		"2024-03-20T03:07:00",
+		"2025-03-20T09:02:00",
+		"2026-03-20T14:46:00",
+		"2027-03-20T20:25:00",
+		"2028-03-20T02:17:00",
+		"2029-03-20T08:01:00",
+	)
+	@ParameterizedTest fun `equinox should be the 0 point, every year`(dateTime: LocalDateTime) {
+		val result = subject.approximateSolarLongitude(dateTime)
+
+		result shouldBe 0.0
+	}
+}
