ae1b5e95b34ecb13bc8525f7d437d80a0e3a931f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-08 22:06:06 +0100

    Add template activity

---
+++ modules/bazi-app/src/main/java/net/twisterrob/bazi/ui/theme/Theme.kt
@@ -0,0 +1,59 @@
+package net.twisterrob.bazi.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+	primary = Purple80,
+	secondary = PurpleGrey80,
+	tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+	primary = Purple40,
+	secondary = PurpleGrey40,
+	tertiary = Pink40
+
+	/* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun AppTheme(
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	dynamicColor: Boolean = true,
+	content: @Composable () -> Unit,
+) {
+	val colorScheme = when {
+		dynamicColor ->
+			if (darkTheme)
+				dynamicDarkColorScheme(LocalContext.current)
+			else
+				dynamicLightColorScheme(LocalContext.current)
+
+		darkTheme ->
+			DarkColorScheme
+
+		else ->
+			LightColorScheme
+	}
+
+	MaterialTheme(
+		colorScheme = colorScheme,
+		typography = Typography,
+		content = content
+	)
+}
