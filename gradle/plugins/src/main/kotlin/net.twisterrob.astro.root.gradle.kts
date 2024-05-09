dcdf00b190bbf002465cc13818133c73f9e276b6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-09 08:47:39 +0100

    Move detekt into plugin

--- gradle/plugins/src/main/kotlin/net.twisterrob.astro.root.gradle.kts
+++ gradle/plugins/src/main/kotlin/net.twisterrob.astro.root.gradle.kts
@@ -1,28 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
-import io.gitlab.arturbosch.detekt.report.ReportMergeTask
-
-val detektReportMergeSarif: TaskProvider<ReportMergeTask> = tasks
-	.register("detektReportMergeSarif", ReportMergeTask::class.java) {
-		output = project.layout.buildDirectory.file("reports/detekt/merge.sarif")
-	}
-
-subprojects {
-	plugins.withId("io.gitlab.arturbosch.detekt") {
-
-		tasks.named("check").configure {
-			dependsOn(tasks.withType<Detekt>())
-		}
-
-		tasks.named("detekt").configure {
-			enabled = false
-		}
-
-		tasks.withType<Detekt>().configureEach {
-			reports.sarif.required = true
-			finalizedBy(detektReportMergeSarif)
-		}
-		detektReportMergeSarif.configure {
-			input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
-		}
-	}
+plugins {
+	id("net.twisterrob.astro.build.detektReportMerge")
 }
---
+++ gradle/plugins/src/main/kotlin/net/twisterrob/astro/build/detektReportMerge.gradle.kts
@@ -0,0 +1,32 @@
+package net.twisterrob.astro.build
+
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.withType
+
+val detektReportMergeSarif: TaskProvider<ReportMergeTask> = tasks
+	.register("detektReportMergeSarif", ReportMergeTask::class.java) {
+		output = project.layout.buildDirectory.file("reports/detekt/merge.sarif")
+	}
+
+subprojects {
+	plugins.withId("io.gitlab.arturbosch.detekt") {
+
+		tasks.named("check").configure {
+			dependsOn(tasks.withType<Detekt>())
+		}
+
+		tasks.named("detekt").configure {
+			enabled = false
+		}
+
+		tasks.withType<Detekt>().configureEach {
+			reports.sarif.required = true
+			finalizedBy(detektReportMergeSarif)
+		}
+		detektReportMergeSarif.configure {
+			input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
+		}
+	}
+}
