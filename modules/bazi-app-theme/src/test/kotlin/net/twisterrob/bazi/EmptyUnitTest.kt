2cdfb4469add69047f780bb8a0de74b61e442de2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-09 10:13:07 +0100

    Add test to satisfy Gradle 9

---
+++ modules/bazi-app-theme/src/test/kotlin/net/twisterrob/bazi/EmptyUnitTest.kt
@@ -0,0 +1,9 @@
+package net.twisterrob.bazi
+
+import org.junit.Test
+
+class EmptyUnitTest {
+	@Test fun nothing() {
+		// Empty test to make sure the test folder is compiled and Gradle doesn't blow up.
+	}
+}
