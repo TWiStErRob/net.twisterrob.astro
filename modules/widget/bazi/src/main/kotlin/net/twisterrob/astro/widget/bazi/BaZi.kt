e41b98326ba93a5fc0ba90f8f450b03abcd99940
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-11 12:43:20 +0100

    Add hour column to hourless view

--- modules/widget/bazi/src/main/kotlin/net/twisterrob/astro/widget/bazi/BaZi.kt
+++ modules/widget/bazi/src/main/kotlin/net/twisterrob/astro/widget/bazi/BaZi.kt
@@ -1,15 +1,17 @@
 package net.twisterrob.astro.widget.bazi
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.twisterrob.astro.bazi.model.BaZi
 import net.twisterrob.astro.bazi.model.EarthlyBranch
 import net.twisterrob.astro.bazi.model.HeavenlyStem
@@ -48,9 +50,20 @@ public fun BaZi(bazi: BaZi, modifier: Modifier = Modifier) {
 				modifier = space25percent,
 			)
 		} else {
-			Box(
+			Column(
 				modifier = space25percent,
-			)
+				horizontalAlignment = CenterHorizontally,
+			) {
+				Text(
+					text = "Hour",
+				)
+				Text(
+					text = "Unknown",
+					modifier = Modifier
+						.padding(top = 60.dp)
+						.rotate(@Suppress("detekt.MagicNumber") -75f),
+				)
+			}
 		}
 	}
 }
