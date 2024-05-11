9a10b196a1846ccc39091b5c01bd5a442135a60e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-11 10:39:17 +0100

    Fix JulianDay's type

--- modules/domain/bazi-calculator/solar/src/main/kotlin/net/twisterrob/astro/units/JulianDay.kt
+++ modules/domain/bazi-calculator/solar/src/main/kotlin/net/twisterrob/astro/units/JulianDay.kt
@@ -2,11 +2,14 @@
 
 package net.twisterrob.astro.units
 
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+
 @JvmInline
 internal value class JulianDay(internal val value: Double)
 
 internal inline val Double.jd: JulianDay
 	get() = JulianDay(this)
 
-internal inline operator fun JulianDay.minus(other: JulianDay): Double =
-	this.value - other.value
+internal inline operator fun JulianDay.minus(other: JulianDay): Duration =
+	(this.value - other.value).days
