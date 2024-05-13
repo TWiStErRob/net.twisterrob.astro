9f6ddf887df6dbe0e29295e8da8425e48ec7fe3d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-13 08:54:52 +0100

    Test all scenarios

--- modules/widget/bazi/src/sharedTest/kotlin/net/twisterrob/astro/widget/bazi/BaZiChartUiTest.kt
+++ modules/widget/bazi/src/sharedTest/kotlin/net/twisterrob/astro/widget/bazi/BaZiChartUiTest.kt
@@ -77,7 +77,85 @@ class BaZiChartUiTest {
 
 	@Test
 	fun `year add is clickable`() {
-		val mockListener: () -> Unit = mock()
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Year", "add")
+
+		verify(mockListeners.onYearAdd).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `year subtract is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Year", "subtract")
+
+		verify(mockListeners.onYearSubtract).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `month add is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Month", "add")
+
+		verify(mockListeners.onMonthAdd).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `month subtract is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Month", "subtract")
+
+		verify(mockListeners.onMonthSubtract).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `day add is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Day", "add")
+
+		verify(mockListeners.onDayAdd).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `day subtract is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Day", "subtract")
+
+		verify(mockListeners.onDaySubtract).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `hour add is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Hour", "add")
+
+		verify(mockListeners.onHourAdd).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	@Test
+	fun `hour subtract is clickable`() {
+		val mockListeners = setContentWithListeners(MockListeners())
+
+		compose.clickPillarAction("Hour", "subtract")
+
+		verify(mockListeners.onHourSubtract).invoke()
+		mockListeners.verifyNoMoreInteractions()
+	}
+
+	private fun setContentWithListeners(mockListeners: MockListeners): MockListeners {
 		compose.setContent {
 			BaZiChart(
 				bazi = BaZi(
@@ -86,19 +164,42 @@ class BaZiChartUiTest {
 					day = BaZi.Pillar(HeavenlyStem.Bing, EarthlyBranch.Yin),
 					hour = BaZi.Pillar(HeavenlyStem.Ding, EarthlyBranch.Mao),
 				),
-				onYearAdd = mockListener,
-				onYearSubtract = {},
-				onMonthAdd = {},
-				onMonthSubtract = {},
-				onDayAdd = {},
-				onDaySubtract = {},
-				onHourAdd = {},
-				onHourSubtract = {},
+				onYearAdd = mockListeners.onYearAdd,
+				onYearSubtract = mockListeners.onYearSubtract,
+				onMonthAdd = mockListeners.onMonthAdd,
+				onMonthSubtract = mockListeners.onMonthSubtract,
+				onDayAdd = mockListeners.onDayAdd,
+				onDaySubtract = mockListeners.onDaySubtract,
+				onHourAdd = mockListeners.onHourAdd,
+				onHourSubtract = mockListeners.onHourSubtract,
 			)
 		}
-		compose.clickPillarAction("Year", "add")
-		verify(mockListener).invoke()
-		verifyNoMoreInteractions(mockListener)
+		return mockListeners
+	}
+
+	@Suppress("detekt.LongParameterList")
+	private class MockListeners(
+		val onYearAdd: () -> Unit = mock(),
+		val onYearSubtract: () -> Unit = mock(),
+		val onMonthAdd: () -> Unit = mock(),
+		val onMonthSubtract: () -> Unit = mock(),
+		val onDayAdd: () -> Unit = mock(),
+		val onDaySubtract: () -> Unit = mock(),
+		val onHourAdd: () -> Unit = mock(),
+		val onHourSubtract: () -> Unit = mock(),
+	) {
+		fun verifyNoMoreInteractions() {
+			verifyNoMoreInteractions(
+				onYearAdd,
+				onYearSubtract,
+				onMonthAdd,
+				onMonthSubtract,
+				onDayAdd,
+				onDaySubtract,
+				onHourAdd,
+				onHourSubtract,
+			)
+		}
 	}
 }
 
