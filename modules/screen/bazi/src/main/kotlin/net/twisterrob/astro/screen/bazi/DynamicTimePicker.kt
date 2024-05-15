9ec7285f3edaf4694de32ab59b0e8f4e22f316b3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-15 09:24:27 +0100

    Editable Time picker

---
+++ modules/screen/bazi/src/main/kotlin/net/twisterrob/astro/screen/bazi/DynamicTimePicker.kt
@@ -0,0 +1,183 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package net.twisterrob.astro.screen.bazi
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerColors
+import androidx.compose.material3.TimePickerDefaults
+import androidx.compose.material3.TimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalTime
+
+@Composable
+internal fun DynamicTimePicker(
+	state: TimePickerState,
+	modifier: Modifier = Modifier,
+	showModeToggle: Boolean = true,
+	displayMode: DynamicTimePickerDisplayMode = defaultDynamicTimePickerDisplayMode,
+	colors: TimePickerColors = TimePickerDefaults.colors(),
+) {
+	var displayModeState by rememberSaveable { mutableStateOf(displayMode) }
+	Column(
+		modifier = modifier,
+	) {
+		CompositionLocalProvider(
+			LocalContentColor provides AlertDialogDefaults.titleContentColor,
+			LocalTextStyle provides LocalTextStyle.current.merge(
+				other = MaterialTheme.typography.labelLarge
+			)
+		) {
+			val title = when (displayModeState) {
+				DynamicTimePickerDisplayMode.Input -> "Enter time"
+				DynamicTimePickerDisplayMode.Picker -> "Select time"
+			}
+			Row {
+				Text(
+					text = title,
+					modifier = Modifier.padding(start = 24.dp, end = 12.dp, top = 16.dp, bottom = 24.dp)
+				)
+				if (showModeToggle && !isLandscape) {
+					Spacer(modifier = Modifier.weight(1f))
+					DisplayModeToggleButton(
+						modifier = Modifier.padding(end = 12.dp, bottom = 12.dp),
+						displayMode = displayModeState,
+						onDisplayModeChange = { displayModeState = it },
+					)
+				}
+			}
+		}
+		when {
+			displayModeState == DynamicTimePickerDisplayMode.Input || isLandscape ->
+				TimeInput(
+					state = state,
+					modifier = Modifier.align(Alignment.CenterHorizontally),
+					colors = colors,
+				)
+
+			displayModeState == DynamicTimePickerDisplayMode.Picker ->
+				TimePicker(
+					state = state,
+					modifier = Modifier.align(Alignment.CenterHorizontally),
+					colors = colors,
+				)
+		}
+	}
+}
+
+internal enum class DynamicTimePickerDisplayMode {
+	Input,
+	Picker,
+
+	;
+}
+
+private val defaultDynamicTimePickerDisplayMode: DynamicTimePickerDisplayMode
+	@Composable
+	@ReadOnlyComposable
+	get() = if (isLandscape) {
+		DynamicTimePickerDisplayMode.Input
+	} else {
+		DynamicTimePickerDisplayMode.Picker
+	}
+
+private val isLandscape: Boolean
+	@Composable
+	@ReadOnlyComposable
+	get() = with(LocalConfiguration.current) { screenHeightDp < screenWidthDp }
+
+@Composable
+private fun DisplayModeToggleButton(
+	displayMode: DynamicTimePickerDisplayMode,
+	onDisplayModeChange: (DynamicTimePickerDisplayMode) -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	if (displayMode == DynamicTimePickerDisplayMode.Picker) {
+		IconButton(onClick = { onDisplayModeChange(DynamicTimePickerDisplayMode.Input) }, modifier = modifier) {
+			Icon(
+				imageVector = Filled.Edit,
+				contentDescription = "Switch to text input mode"
+			)
+		}
+	} else {
+		IconButton(onClick = { onDisplayModeChange(DynamicTimePickerDisplayMode.Picker) }, modifier = modifier) {
+			Icon(
+				imageVector = Filled.DateRange,
+				contentDescription = "Switch to clock input mode"
+			)
+		}
+	}
+}
+
+@Preview
+@Composable
+private fun DynamicTimePickerPickerPreview() {
+	DynamicTimePicker(
+		state = TimePickerState.now(),
+		displayMode = DynamicTimePickerDisplayMode.Picker,
+	)
+}
+
+@Preview
+@Composable
+private fun DynamicTimePickerPickerFixedPreview() {
+	DynamicTimePicker(
+		state = TimePickerState.now(),
+		displayMode = DynamicTimePickerDisplayMode.Picker,
+		showModeToggle = false,
+	)
+}
+
+@Preview
+@Composable
+private fun DynamicTimePickerInputPreview() {
+	DynamicTimePicker(
+		state = TimePickerState.now(),
+		displayMode = DynamicTimePickerDisplayMode.Input,
+	)
+}
+
+@Preview
+@Composable
+private fun DynamicTimePickerInputFixedPreview() {
+	DynamicTimePicker(
+		state = TimePickerState.now(),
+		displayMode = DynamicTimePickerDisplayMode.Input,
+		showModeToggle = false,
+	)
+}
+
+private fun TimePickerState.Companion.now(): TimePickerState {
+	val time = LocalTime.now()
+	return TimePickerState(
+		initialHour = time.hour,
+		initialMinute = time.minute,
+		is24Hour = false,
+	)
+}
--- modules/screen/bazi/src/main/kotlin/net/twisterrob/astro/screen/bazi/TimePickerDialog.kt
+++ modules/screen/bazi/src/main/kotlin/net/twisterrob/astro/screen/bazi/TimePickerDialog.kt
@@ -1,26 +1,13 @@
 package net.twisterrob.astro.screen.bazi
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import net.twisterrob.astro.component.theme.AppTheme
 import java.time.LocalTime
 import java.time.ZonedDateTime
@@ -47,27 +34,9 @@ internal fun TimePickerDialog(
 			TextButton(onHideTimePicker) { Text("Cancel") }
 		}
 	) {
-		Column(
-			modifier = Modifier.fillMaxWidth(),
-		) {
-			CompositionLocalProvider(
-				LocalContentColor provides AlertDialogDefaults.titleContentColor,
-				LocalTextStyle provides LocalTextStyle.current.merge(
-					other = MaterialTheme.typography.labelLarge
-				)
-			) {
-				Box(contentAlignment = Alignment.BottomStart) {
-					Text(
-						text = "Select time",
-						modifier = Modifier.padding(start = 24.dp, end = 12.dp, top = 16.dp, bottom = 24.dp)
-					)
-				}
-			}
-			TimePicker(
-				state = pickerState,
-				modifier = Modifier.align(Alignment.CenterHorizontally)
-			)
-		}
+		DynamicTimePicker(
+			state = pickerState,
+		)
 	}
 }
 
