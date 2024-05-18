66a9911cfb01c0298c78aa9e96ef5d66c0132509
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-18 21:04:10 +0100

    Docs

--- modules/component/test-base-screenshot/src/main/kotlin/net/twisterrob/astro/test/fixtures/TestInstants.kt
+++ modules/component/test-base-screenshot/src/main/kotlin/net/twisterrob/astro/test/fixtures/TestInstants.kt
@@ -5,8 +5,15 @@ import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
+/**
+ * Test instants for consistent testing.
+ */
+@Suppress("detekt.MagicNumber")
 public object TestInstants {
 
+	/**
+	 * The birthday of this repository.
+	 */
 	public val REPO: ZonedDateTime = ZonedDateTime.of(
 		LocalDate.of(2024, 4, 20),
 		LocalTime.of(14, 20, 31),
