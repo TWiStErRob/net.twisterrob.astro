41ac355ef7f0acedc82946a854bdd54f5fa65cf9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-18 18:57:48 +0100

    Add more tests

---
+++ modules/feature/main/src/screenshotTest/kotlin/net/twisterrob/astro/widget/bazi/chart/MainScreenScreenshots.kt
@@ -0,0 +1,17 @@
+//package net.twisterrob.astro.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import net.twisterrob.astro.component.theme.AppTheme
+import net.twisterrob.astro.feature.main.MainScreen
+
+class MainScreenScreenshots {
+
+	@Preview
+	@Composable
+	private fun Default() {
+		AppTheme {
+			MainScreen()
+		}
+	}
+}
---
+++ modules/screen/bazi/src/screenshotTest/kotlin/net/twisterrob/astro/widget/bazi/chart/BaZiScreenScreenshots.kt
@@ -0,0 +1,17 @@
+//package net.twisterrob.astro.screen.bazi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import net.twisterrob.astro.component.theme.AppTheme
+import net.twisterrob.astro.screen.bazi.BaZiScreen
+
+class BaZiScreenScreenshots {
+
+	@Preview
+	@Composable
+	private fun Default() {
+		AppTheme {
+			BaZiScreen()
+		}
+	}
+}
