9185034ecb9ece5171fc617f88da806f9bf1fcff
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-19 18:26:52 +0100

    Fake failure

--- modules/screen/bazi/src/screenshotTest/kotlin/net/twisterrob/astro/widget/bazi/chart/BaZiScreenScreenshots.kt
+++ modules/screen/bazi/src/screenshotTest/kotlin/net/twisterrob/astro/widget/bazi/chart/BaZiScreenScreenshots.kt
@@ -2,18 +2,15 @@
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import net.twisterrob.astro.component.theme.AppTheme
 import net.twisterrob.astro.screen.bazi.BaZiScreen
-import net.twisterrob.astro.screen.bazi.BaZiViewModel
-import net.twisterrob.astro.test.fixtures.TestInstants
 
 class BaZiScreenScreenshots {
 
 	@Preview
 	@Composable
 	private fun Default() {
-		viewModel<BaZiViewModel>().select(TestInstants.REPO)
+		//viewModel<BaZiViewModel>().select(TestInstants.REPO)
 		AppTheme {
 			BaZiScreen()
 		}
