70d50ee4d52ae3290cd769b615aafec4c7b044b5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-05-31 07:56:04 +0100

    Shared config

--- gradle/plugins/src/main/kotlin/net.twisterrob.astro.library.gradle.kts
+++ gradle/plugins/src/main/kotlin/net.twisterrob.astro.library.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
 	id("org.gradle.java-library")
 	id("org.jetbrains.kotlin.jvm")
+	id("com.android.lint")
 	id("net.twisterrob.astro.build.kotlin")
 	id("net.twisterrob.astro.build.testing-java")
 	id("net.twisterrob.astro.build.detekt")
+	id("net.twisterrob.astro.build.lint")
 }
--- gradle/plugins/src/main/kotlin/net/twisterrob/astro/build/lint.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/astro/build/lint.gradle.kts
@@ -1,13 +1,19 @@
 package net.twisterrob.astro.build
 
+import com.android.build.api.dsl.Lint
 import net.twisterrob.astro.build.dsl.android
+import net.twisterrob.astro.build.dsl.lint
 
 plugins.withId("com.android.base") {
-	android {
-		lint {
-			warningsAsErrors = true
-			checkAllWarnings = true
-			lintConfig = rootProject.file("config/lint/lint.xml")
-		}
-	}
+	android.lint(Lint::configure)
+}
+
+plugins.withId("com.android.lint") {
+	lint(Lint::configure)
+}
+
+private fun Lint.configure() {
+	warningsAsErrors = true
+	checkAllWarnings = true
+	lintConfig = rootProject.file("config/lint/lint.xml")
 }
