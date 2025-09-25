27e1a4746d4cf622ebc83ea760bcca3e77e7c160
Róbert Papp <papp.robert.s@gmail.com>
2025-09-25 23:18:40 +0100

    Add missing :component:compose tests (isLandscape/isPortrait/defaultLocale) (#274)

---
+++ modules/component/compose/src/sharedTest/kotlin/net/twisterrob/astro/compose/IsLandscapeTest.kt
@@ -0,0 +1,45 @@
+package net.twisterrob.astro.compose
+
+import android.content.res.Configuration
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class IsLandscapeTest {
+
+	@get:Rule
+	val composeTestRule = createComposeRule()
+
+	@Test
+	fun localConfiguration_landscape() {
+		composeTestRule.setContent {
+			CompositionLocalProvider(
+				LocalConfiguration provides Configuration().apply {
+					orientation = Configuration.ORIENTATION_LANDSCAPE
+				}
+			) {
+				assertTrue(isLandscape)
+			}
+		}
+	}
+
+	@Test
+	fun localConfiguration_portrait() {
+		composeTestRule.setContent {
+			CompositionLocalProvider(
+				LocalConfiguration provides Configuration().apply {
+					orientation = Configuration.ORIENTATION_PORTRAIT
+				}
+			) {
+				assertFalse(isLandscape)
+			}
+		}
+	}
+}
