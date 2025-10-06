79f54c968ddaa96bdb08b98d5ea2730dc86ef44a
Róbert Papp <papp.robert.s@gmail.com>
2025-10-06 02:45:39 +0100

    Remove Greeting module and add missing Preview (#288)

--- modules/component/compose/src/main/kotlin/net/twisterrob/astro/compose/preview/PreviewOrientation.kt
+++ modules/component/compose/src/main/kotlin/net/twisterrob/astro/compose/preview/PreviewOrientation.kt
@@ -1,7 +1,12 @@
 package net.twisterrob.astro.compose.preview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import net.twisterrob.astro.compose.isLandscape
+import net.twisterrob.astro.compose.isPortrait
 
 /**
  * Annotate a preview to show it in both orientations.
@@ -14,3 +19,12 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(name = "Phone - Portrait", device = Devices.PHONE)
 @Preview(name = "Phone - Landscape", device = "spec:width=411dp, height=891dp, orientation=landscape, dpi=420")
 public annotation class PreviewOrientation
+
+@PreviewOrientation
+@Composable
+private fun PreviewOrientationPreview() {
+	Column {
+		Text("isLandscape = ${isLandscape}")
+		Text("isPortrait = ${isPortrait}")
+	}
+}
